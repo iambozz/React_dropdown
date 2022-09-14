8e1affdfd493072c06dc9c160e5658fbb909ff47
iambozz <54217522+iambozz@users.noreply.github.com>
2022-09-14 11:40:06 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Menu from "./components/Menu";
+
+export default function App(): JSX.Element {
+  return (
+    <div className="app">
+      <Menu />
+    </div>
+  );
+}
